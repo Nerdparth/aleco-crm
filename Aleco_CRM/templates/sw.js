9d0e2acf14d6f3c64de1560f9905e2beb5c24077
Nerdparth <parthkapoor488@gmail.com>
2025-03-24 05:13:37 +0530

    added pwa configuration

--- Aleco_CRM/templates/sw.js
+++ Aleco_CRM/templates/sw.js
@@ -1,19 +1,22 @@
 self.addEventListener("install", (event) => {
-    event.waitUntil(
-      caches.open("django-pwa").then((cache) => {
-        return cache.addAll([
-          "/",
-          "/static/pwa/image.png", // Icons
-        ]);
+  event.waitUntil(
+      caches.keys().then((cacheNames) => {
+          return Promise.all(cacheNames.map((cache) => caches.delete(cache))); // Delete all caches
       })
-    );
-  });
-  
-  self.addEventListener("fetch", (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
+  );
+  self.skipWaiting(); // Force activation immediately
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+      fetch(event.request).then((response) => {
+          return response; // Always fetch fresh data
+      }).catch(() => {
+          return new Response("Network unavailable", { status: 503 });
       })
-    );
-  });
-  
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim()); // Ensure new service worker takes over
+});
